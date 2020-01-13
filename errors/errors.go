e7e65eae49aae16813053b788deeb17ced78e08e
Lando-Li <1546737374@qq.com>
2020-01-13 21:19:37 +0800

    完成基础数据类型和自定义错误的定义

---
+++ errors/errors.go
@@ -0,0 +1,71 @@
+package errors
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+// ErrorType 代表错误的类型
+type ErrorType string
+
+// 错误类型的常量
+const (
+	// 下载器错误
+	ERROR_TYPE_DOWNLOADER ErrorType = "downloader error"
+	// 分析器错误
+	ERROR_TYPE_ANALYER ErrorType = "analyzer error"
+	// 条目处理管道错误
+	ERROR_TYPE_PIPELINE ErrorType = "pipeline error"
+	// 调度器错误
+	ERROR_TYPE_SCHEDULER ErrorType = "scheduler error"
+)
+
+// 爬虫错误的接口类型
+type CrawlerError interface {
+	// 用于获取错误的类型
+	Type() ErrorType
+	// 用于获取错误提示信息
+	Error() string
+}
+
+// 爬虫错误的实现类型
+type myCrawlerError struct {
+	// 错误的类型
+	errType ErrorType
+	// 错误的提示信息
+	errMsg string
+	// 完整的错误提示信息
+	fullErrMsg string
+}
+
+// 用于创建一个新的爬虫错误值
+func NewCrawlerError(errType ErrorType, errMsg string) CrawlerError {
+	return &myCrawlerError{
+		errType: errType,
+		errMsg:  strings.TrimSpace(errMsg),
+	}
+}
+
+func (ce *myCrawlerError) Type() ErrorType {
+	return ce.errType
+}
+
+func (ce *myCrawlerError) Error() string {
+	if ce.fullErrMsg == "" {
+		ce.genFullErrMsg()
+	}
+	return ce.fullErrMsg
+}
+
+// 用于生成错误提示信息，并给相应的字段赋值
+func (ce *myCrawlerError) genFullErrMsg() {
+	var buffer bytes.Buffer
+	buffer.WriteString("crawler error: ")
+	if ce.errType != "" {
+		buffer.WriteString(string(ce.errType))
+		buffer.WriteString(" : ")
+	}
+	buffer.WriteString(ce.errMsg)
+	ce.fullErrMsg = fmt.Sprintf("%s", buffer.String())
+}
--- module/data.go
+++ module/data.go
@@ -1,1 +1,79 @@
 package module
+
+import "net/http"
+
+// 数据的接口类型
+type Data interface {
+	// 用于判断数据是否有效
+	Valid() bool
+}
+
+// 数据请求类型
+type Request struct {
+	// HTTP 请求
+	httpReq *http.Request
+	// 请求的深度
+	depth uint32
+}
+
+// 用于创建一个新的请求实例
+func NewRequest(httpReq *http.Request, depth uint32) *Request {
+	return &Request{
+		httpReq: httpReq,
+		depth:   depth,
+	}
+}
+
+// 用于获取HTTP请求
+func (req *Request) HTTPReq() *http.Request {
+	return req.httpReq
+}
+
+// 用于获取请求的深度
+func (req *Request) Depth() uint32 {
+	return req.depth
+}
+
+// 用于判断请求是否有效
+func (req *Request) Valid() bool {
+	return req.httpReq != nil && req.httpReq.URL != nil
+}
+
+// 数据响应的类型
+type Response struct {
+	// HTTP响应
+	httpResp *http.Response
+	// 响应的深度
+	depth uint32
+}
+
+// 用于创建一个新的响应类型
+func NewResponse(httpResp *http.Response, depth uint32) *Response {
+	return &Response{
+		httpResp: httpResp,
+		depth:    depth,
+	}
+}
+
+// 用于获取HTTP响应
+func (resp *Response) HTTPResp() *http.Response {
+	return resp.httpResp
+}
+
+//用于获取请求的深度
+func (resp *Response) Depth() uint32 {
+	return resp.depth
+}
+
+// 用于判断响应是否有效
+func (resp *Response) Valid() bool {
+	return resp.httpResp != nil && resp.httpResp.Body != nil
+}
+
+// 条目的类型
+type Item map[string]interface{}
+
+// 用于判断条目是否有效
+func (item Item) Valid() bool {
+	return item != nil
+}
